6706f13b6d4a97af0bb175c6fb479cf1853392e7
EdumbaP <edumba@gmail.com>
2018-11-18 20:55:03 +1100

    Added ServiceCollection and Settings File

---
+++ MovieProcessor/MovieProcessorSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieProcessor
+{
+    class MovieProcessorSettings
+    {
+        public string ApiKey { get; set; }
+        public string BaseURL { get; set; }
+        public string MovieListRelativeURL { get; set; }
+        public string MovieListFallbackRelativeURL { get; set; }
+        public string MovieDetailRelativeURL { get; set; }
+        public string MovieDetailFallbackRelativeURL { get; set; }
+    }
+}
